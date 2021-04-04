2962cee699fa35ab7c36f7a8556a2f1a8f1c2250
Valdismin <Valdismin@gmail.com>
2021-04-04 15:11:24 +0300

    Add Routes. Add Lost Password Page.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `https://neko-back.herokuapp.com/2.0`,
+    baseURL: `https://neko-back.herokuapp.com/2.0/`,
     withCredentials: true,
 })
 
 export const lostPasswordAPI = {
     postEmail(email: string) {
-        return instance.post(`auth/forgot`, {email, from: "cards-admin <valdismin@gmail.com>", message: `<div style="background-color: lime; padding: 15px"> password recovery link: <a href='https://Valdismin.github.io/cards/#/newpassword/$token$'>link</a></div>`}).then(response => response.data)
+        return instance.post(`auth/forgot`, {email, from: "cards-admin <valdismin@gmail.com>", message: `<div style="background-color: lime; padding: 15px"> password recovery link: <a href='https://Valdismin.github.io/friday_project/#/new-pass/$token$'>link</a></div>`}).then(response => response.data)
     }
 }
 export const newPasswordAPI = {
